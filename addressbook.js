d0046d5e2b4e9f3af95704025742fad09c42b1f3
tenznz <dhugkar@gmail.com>
2022-01-05 09:38:37 -0800

    Added Regex to check validation

--- addressbook.js
+++ addressbook.js
@@ -12,42 +12,66 @@ class Contact {
 
     get firstname() { return this._firstname; }
     set firstname(firstname) {
+        let pattern1 = RegExp('^[A-Z]{1}[a-z]{2,}$');
+        if (pattern1.test(firstname))
             this._firstname = firstname;
+        else console.log('FirstName is Incorrect!');
     }
 
     get lastname() { return this._lastname; }
     set lastname(lastname) {
+        let pattern2 = RegExp('^[A-Z]{1}[a-z]{2,}$');
+        if (pattern2.test(lastname))
             this._lastname = lastname;
+        else console.log('LastName is Incorrect!');
     }
 
     get address() { return this._address; }
     set address(address) {
+        let pattern3 = RegExp('^[A-Z]{1}[a-z]{4,}$');
+        if (pattern3.test(address))
             this._address = address;
+        else console.log('Address is Incorrect!');
     }
 
     get city() { return this._city; }
     set city(city) {
+        let pattern4 = RegExp('^[A-Z]{1}[a-z]{4,}$');
+        if (pattern4.test(city))
             this._city = city;
+        else console.log('City is Incorrect!');
     }
 
     get state() { return this._state; }
     set state(state) {
+        let pattern5 = RegExp('^[A-Z]{1}[a-z]{2,}$');
+        if (pattern5.test(state))
             this._state = state;
+        else console.log('State is Incorrect!');
     }
 
     get zipcode() { return this._zipcode; }
     set zipcode(zipcode) {
+        let pattern6 = RegExp('^[0-9]{3}?[\\s,-]{0,1}[0-9]{3}$');
+        if (pattern6.test(zipcode))
             this._zipcode = zipcode;
+        else console.log('Zipcode is Incorrect!');
     }
 
     get phoneNumber() { return this._phoneNumber; }
     set phoneNumber(phoneNumber) {
+        let pattern7 = RegExp('^[+][0-9]{2}\\s?[1-9]{1}[0-9]{9}$');
+        if (pattern7.test(phoneNumber))
             this._phoneNumber = phoneNumber;
+        else console.log('PhoneNumber is Incorrect!');
     }
 
     get emailId() { return this._emailId; }
     set emailId(emailId) {
+        let pattern8 = RegExp('^[0-9a-zA-Z+-._]+@[0-9a-zA-Z]+.[a-zA-Z]{2,3}.([a-zA-z]{2,3})*$');
+        if (pattern8.test(emailId))
             this._emailId = emailId;
+        else console.log('EmailId is Incorrect!');
     }
 
     toString() {
@@ -57,5 +81,5 @@ class Contact {
     }
 }
 
-let personContact = new Contact("ten", "duk", "India", "banglore", "Karnataka", "123", "7018485591", "dhugkar@gmail.com");
+let personContact = new Contact("Ten", "Duk", "India", "Banglore", "Karnataka", "123 122", "+91 7018485591", "dhugkar@gmail.com");
 console.log(personContact.toString());
